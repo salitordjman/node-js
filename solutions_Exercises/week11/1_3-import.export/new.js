490bfd8da5090d04a600a5ae73cf4fb33c29be70
salitordjman <salitorgi@gmail.com>
2022-01-09 16:29:05 +0200

    fgrfg

---
+++ solutions_Exercises/week11/1_3-import.export/new.js
@@ -0,0 +1,11 @@
+const func1 = () => {
+  console.log("aaa111111");
+};
+
+const func2 = () => {
+  console.log("bbb2222222");
+};
+const func3 = () => {
+  console.log("cccc33333333333");
+};
+export { func1, func2, func3 };
