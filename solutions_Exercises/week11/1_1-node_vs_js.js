edf743a264c8353f10f73e8bce37745019407afc
salitordjman <salitorgi@gmail.com>
2022-01-09 12:56:57 +0200

    start

---
+++ solutions_Exercises/week11/1_1-node_vs_js.js
@@ -0,0 +1,13 @@
+//!node vs the browser
+
+//in node there is the process and global object
+//?in the browser there is the window and document object
+
+//in node we can set the enviroment
+//meaning that we dont care what is the browSwer type
+
+//in node there is no DOM
+//and no CSS
+
+//node is non-blocking
+//neaning the I-O operations in node are async
